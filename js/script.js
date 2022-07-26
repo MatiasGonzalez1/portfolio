474b1c32f65b521c6d7be894a9aae339344d731e
Matias Gonzalez <mng_epson@hotmail.com>
2022-07-26 00:24:18 -0300

    feat: add dark theme

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/** typing animation*/
+
+const typed = new Typed(".typing", {
+    strings:[" "," ","Web developer","Desarrollador web","Photograph", "Fotografo", "Content creator", "Creador de contenido"],
+    typeSpeed:80,
+    backSpeed:40,
+    loop:true
+})
